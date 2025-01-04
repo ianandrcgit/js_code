36a93f733849662f1b93da90d30f1bf8834e6775
Anand R C <118249456+ianandrcgit@users.noreply.github.com>
2025-01-04 11:05:13 +0000

    Variable and Datatype

---
+++ js_code/var_datatype.js
@@ -0,0 +1,15 @@
+/*1. Variables and Data Types
+Problem 1: Create a simple user profile
+Create variables for storing user data (e.g., name, age, and email) and log them to the console.
+
+Steps:
+
+Create a variable for name, age, and email.
+Use console.log() to display the values in the console.
+Why this helps React: React components often use variables to store and render user data (like name, age, etc.).*/
+
+let name = "Anand";
+let age = 35;
+let email = "anand@example.com";
+
+console.log(name, age, email);
