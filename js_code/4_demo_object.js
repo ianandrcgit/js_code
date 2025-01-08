3cb0e46e4105b9f8e4314838aace260295aa6533
Anand R C <118249456+ianandrcgit@users.noreply.github.com>
2025-01-08 14:09:40 +0530

    Add files via upload

---
+++ js_code/4_demo_object.js
@@ -0,0 +1,19 @@
+/*Problem 4: Create an object to represent a product
+Create an object product that has properties like name, price, and availability. Then log these properties to the console.
+
+Steps:
+
+Create an object product with properties name, price, and availability.
+Log each property to the console.
+Why this helps React: Objects are often used to store and manage the data that you render in React components (like product data or user profiles).
+
+*/
+let product = {
+  name: "Laptop",
+  manufacturer: "HP",
+  year_man: 2005,
+};
+
+console.log("Product Name : ", product.name);
+console.log("Product Manufacturer : ", product.manufacturer);
+console.log("Product Year of Manufacture : ", product.year_man);
